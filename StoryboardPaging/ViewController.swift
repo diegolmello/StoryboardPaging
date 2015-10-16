67cb49bb22f9dd8a4a4ddf5eada6e4fb635a4c18
Diego Mello <diegolmello@gmail.com>
2015-10-15 21:58:36 -0300

    Initial Commit

---
+++ StoryboardPaging/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StoryboardPaging
+//
+//  Created by Diego Mello on 10/15/15.
+//  Copyright © 2015 Diego Mello. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
